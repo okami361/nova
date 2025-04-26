f29f753d0498973254243eab82a79494f0e3614f
Vinicius Yu Okubo <vinnco@hotmail.com>
2025-04-26 01:24:16 -0300

    removing debug prints

--- trainer.py
+++ trainer.py
@@ -267,16 +267,8 @@ class Trainer():
 
                 # Runs the forward pass with autocasting.
                 with autocast():
-                    print("Input shape:", inputs.shape)
-                    print("Min / Max :", inputs.min().item(), "/", inputs.max().item())
-                    print("Has NaNs? :", torch.isnan(inputs).any().item())
-                    print("Has infs? :", torch.isinf(inputs).any().item())
-
                     outputs = self.model(inputs)                 # forward pass
                     loss = self.loss_criterion(outputs, labels)  # compute loss
-                    print(f"Loss: {loss.item():.4f}")
-                    print(f"Labels: {labels}")
-                    print(f"Outputs: {outputs}")
                 if parallel:
                     loss = loss.mean()          # list in this case
                 scaler.scale(loss).backward()   # backward() on scaled loss for scaled gradients.        
